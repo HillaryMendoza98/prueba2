986b9063676cd1ebe00b219c48e64acd3e0448c3
hillary <hemc1998@gmail.com>
2024-09-05 23:53:58 -0600

    agregue practica2

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+// Verificar si un Número es Par o Impar
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} es un número par.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} es un número impar.");
+        }
+    }
+}
